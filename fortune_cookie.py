68d6f9715f0e1dbbb12d82c8cfaf9c123e3896d6
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2022-07-22 22:11:04 -0400

    Added pronoun_replace function!

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -6,11 +6,47 @@ from spacy.matcher import Matcher
 import textacy
 from textacy import extract
 import language_tool_python
-
 import test_cases
 
 tool = language_tool_python.LanguageTool('en-US')
 
+
+def pronoun_replace(original_document):
+
+    nlp_doc = nlp(original_document)
+
+    pronoun_types_present = []
+
+    for token in nlp(nlp_doc):
+        if token.pos_ == "PRON":
+            # print(token)
+            for key, val in pronouns_dict.items():
+                # print(key)
+                # print(val)
+                if str(token) in val:
+                    pronoun_types_present.append(key)
+
+    if len(pronoun_types_present)>1:
+        return original_document
+
+    # use loop to replace diff types of pronouns, similar to verb_replace
+    working_doc = ""
+    for token in nlp_doc:
+        token_tag = spacy.explain(token.tag_)
+
+        if token_tag == "pronoun, possessive":
+            working_doc = original_document.replace(str(token), "your")
+        
+        # this may overwrite "you", so I'd need to put it before the
+        # noun_replace function
+        if token_tag == "pronoun, personal":
+            working_doc = original_document.replace(str(token), "yourself")
+                
+    # Only return if replacements have been made
+    if len(working_doc) > 0:
+        return working_doc
+
+    return original_document
 
 def noun_replace(original_document):
     '''Returns a list of documents. Noun chunks that meet the specified
@@ -97,6 +133,39 @@ def verb_replace(original_document):
                 
         # Only return if replacements have been made
         if len(working_doc) > 0:
+            return working_doc
+
+    # Return the original if no replacements have been made.
+    return original_document
+
+def verb_replace_advcl(original_document):
+    nlp_doc = nlp(original_document)
+
+    verb_count = 0
+    for token in nlp_doc:
+        if token.pos_ in ['VERB', 'AUX']:
+            verb_count += 1
+
+    if verb_count == 0:
+        return original_document
+    
+    for token in nlp_doc:
+        token_tag = spacy.explain(token.tag_)
+
+        # Bail-out statements
+        if token.pos_ not in ['VERB', 'AUX']:
+            continue
+
+        if token_tag in ["verb, base form"]:
+            continue
+
+        if "you" not in [token.text for token in token.lefts]:
+            continue
+
+        working_doc = original_document.replace(str(token), token.lemma_)
+                
+        # Only return if replacements have been made
+        if len(working_doc) > 0:
           return working_doc
 
     # Return the original if no replacements have been made.
@@ -122,43 +191,45 @@ def capitalize_first_letter(text):
     return new_text
 
 
-# nlp = spacy.load('en_core_web_sm')
-nlp = spacy.load('en_core_web_trf')
+# nlp = spacy.load('en_core_web_sm')    # small spaCy English language model
+nlp = spacy.load('en_core_web_trf')     # large spaCy English language model
 
-# og_text = 'An RAF squadron is assigned to knock out a German rocket fuel factory in Norway.'
-# og_text = 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.'
-# og_text = 'The ninja find themselves trapped in a pyramid and must escape encroaching lava to warn Ninjago City of a new Serpentine invasion.'
-# og_text ='When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.'
-# og_text = 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
-# og_text = "A cowboy doll is profoundly threatened and jealous when a new spaceman action figure supplants him as top toy in a boy's bedroom."
-# og_text = 'A man raised by gorillas must decide where he really belongs when he discovers he is a human.'
-og_text = "To save her father from death in the army, a young maiden secretly goes in his place and becomes one of China's greatest heroines in the process."
-# og_text = 'An English soldier and the daughter of an Algonquin chief share a romance when English colonists invade seventeenth century Virginia.'
-# og_text = "A poor but hopeful boy seeks one of the five coveted golden tickets that will send him on a tour of Willy Wonka's mysterious chocolate factory."
-
-
+# 
 all_outputs = []
 
 for plot in test_cases.EXAMPLE_SENTENCES:
     nlp_doc = nlp(plot)
-    nouns_replaced = noun_replace(plot)
+    pronouns_replaced = pronoun_replace(plot)
+    nouns_replaced = noun_replace(pronouns_replaced)
 
     current_plot_outputs = []
     for noun_plots in nouns_replaced:
         verbs_replaced = verb_replace(noun_plots)
+        verbs_replaced = verb_replace_advcl(verbs_replaced)
         current_plot_outputs.append(verbs_replaced)
     
     all_outputs.append(current_plot_outputs)
 
 
+for plot in test_cases.EXAMPLE_SENTENCES:
+    pronouns_replaced = pronoun_replace(plot)
+    # print(pronouns_replaced)
+    
+
+
+for i in all_outputs:
+    for j in i:
+        print(j,"\n----------")
 
 
 
+
+
+
+og_text = "To save her father from death in the army, a young maiden secretly goes in his place and becomes one of China's greatest heroines in the process."
 nlp_doc = nlp(og_text)
 
-
 nouns_replaced = noun_replace(og_text)
-
 
 verbs_replaced = verb_replace(nouns_replaced[0])
 
@@ -168,53 +239,30 @@ verbs_replaced = verb_replace(nouns_replaced[0])
 
 
 
-
-
-# for element in nouns_replaced:
-#     print(verb_replace(element))
-
-
-
-
-
-
 # transitive vs reflexive verbs - save her father vs save herself
-# Not sure spaCy can tell if a verb is transitive vs reflexive
+# Not sure spaCy can tell if a verb is transitive vs reflexive - Nope.
 # possessive pronouns maybe
 
+#families of related pronouns:
+pronouns_dict = {"third_male":["he", "him", "his", "himself"]
+                ,"third_female":["she", "her", "hers", "herself"]
+                ,"third_plural":["they", "them", "their", "theirs"
+                                ,"themselves"]}
+
+
+
+
+        
 
 
 
 
 
-for token in nlp(verbs_replaced):
-     print (token, token.lemma_, token.dep_, token.tag_, token.pos_, spacy.explain(token.tag_), token.head)
+for token in nlp(all_outputs[2][0]):
+     print (token, token.lemma_, token.dep_, token.tag_, token.pos_
+            , spacy.explain(token.tag_), token.head)
 
-for token in nlp_doc:
-     print (token, token.lemma_, token.dep_, token.tag_, token.pos_, spacy.explain(token.tag_))
-
-
-for chunk in nlp(verbs_replaced).noun_chunks:
-    print('noun chunk:',chunk
-          ,'\nchunk root:', chunk.root.text
-          ,'\nchunk dependency:', chunk.root.dep_
-          ,'\nroot head text:', chunk.root.head.text
-          ,'\nchunk part of speech',chunk.root.pos_
-          ,'\n-------------------')
-
-
-
-
-
-# print ([token.text for token in introduction_doc])
-
-# for token in introduction_doc:
-#      print (token, token.lemma_, token.dep_, token.tag_, token.pos_, spacy.explain(token.tag_))
-
-
-displacy.serve(nlp_doc, style='dep')
-
-# for chunk in introduction_doc.noun_chunks:
+# for chunk in nlp(verbs_replaced).noun_chunks:
 #     print('noun chunk:',chunk
 #           ,'\nchunk root:', chunk.root.text
 #           ,'\nchunk dependency:', chunk.root.dep_
@@ -224,49 +272,5 @@ displacy.serve(nlp_doc, style='dep')
 
 
 
-
-# Identify the subject of the sentence
-# Find the noun chunk about the subject
-# Change that noun chunk to "You"
-
-# Find pronouns and change them to "yourself"
-
-# Find the AUX verb to that noun chunk
-# Lemmatize the verb
-# Change that AUX verb to the future tense
-# Make a rule that looks for ADV + VERB combo and adds "will" before the adverb,
-## but only if the ADV+VERB combo is related to the subject.
-## Exclude adverbial clauses ^^
-
-
-
-
-
-
-# Expanding named entities and adding a component to the NLP pipeline object
-# Example from https://spacy.io/usage/rule-based-matching#models-rules-ner
-# import spacy
-# from spacy.language import Language
-# from spacy.tokens import Span
-
-# nlp = spacy.load("en_core_web_sm")
-
-# @Language.component("expand_person_entities")
-# def expand_person_entities(doc):
-#     new_ents = []
-#     for ent in doc.ents:
-#         if ent.label_ == "PERSON" and ent.start != 0:
-#             prev_token = doc[ent.start - 1]
-#             if prev_token.text in ("Dr", "Dr.", "Mr", "Mr.", "Ms", "Ms."):
-#                 new_ent = Span(doc, ent.start - 1, ent.end, label=ent.label)
-#                 new_ents.append(new_ent)
-#         else:
-#             new_ents.append(ent)
-#     doc.ents = new_ents
-#     return doc
-
-# # Add the component after the named entity recognizer
-# nlp.add_pipe("expand_person_entities", after="ner")
-
-# doc = nlp("Dr. Alex Smith chaired first board meeting of Acme Corp Inc.")
-# print([(ent.text, ent.label_) for ent in doc.ents])
+# View dependency tree
+displacy.serve(nlp_doc, style='dep')

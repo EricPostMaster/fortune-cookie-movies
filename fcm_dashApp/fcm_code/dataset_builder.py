190c44de2642c158bd2fa51867d8fefbb4dca07d
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2022-08-09 09:16:31 -0400

    Cleaned up unused code

--- fcm_dashApp/fcm_code/dataset_builder.py
+++ fcm_dashApp/fcm_code/dataset_builder.py
@@ -3,32 +3,6 @@ from config import api_key
 from os.path import exists
 import pickle
 
-# Starts with Toy Story
-# toy_story_id = 'tt0114709'
-
-# api_url = 'https://imdb-api.com/en/API/Title'
-
-# response = requests.get(f'{api_url}/{api_key}/{toy_story_id}')
-
-# movie_title = response.json()['title']
-# movie_id = response.json()['id']
-
-# Add Toy Story title to a set of all titles
-# movie_ids = set()
-# movie_ids.add(movie_id)
-
-# Add Toy Story json to all movie data
-# movie_data = {}
-# movie_data[movie_title] = response.json()
-
-# Add similar movies to the set of all titles
-# for similar in response.json()['similars']:
-#     movie_ids.add(similar['id'])
-
-# if there are no keys in the movie data dictionary, start the dict with Toy Story
-# otherwise, check the movie_ids set and add items to the movie data dictionary
-
-
 class MovieDict:
     """
     """
@@ -107,37 +81,10 @@ class MovieDict:
 
 movies = MovieDict()
 
-for i in range(0,20):
+for i in range(0,10):  # Currently duplicating API calls, keep this under 10
     movies.get_movies()
     i+=1
 
 movies.save_movie_data()
 movies.save_movie_ids()
-
-# i=1
-# while i <= 5:
-#     print(i)
-#     movies.get_movies()
-
-
-# Skips Toy Story because it's already in the dataset - GOOD
-# Adds Toy Story 3 because it's new - GOOD
-# Gets hung up on Toy Story 2 because the movie_ids dict changed - PROBLEM
-
-
-
-
-
-    # def add_movie_info(self, api_response):
-    #     movie_title = api_response.json()['title']
-    #     movie_id = api_response.json()['id']
-
-    #     # Add first items to movie_data and movie_ids
-    #     self.movie_data[movie_title] = api_response.json()
-    #     self.movie_ids.add(movie_id)
-
-    #     for similar in api_response.json()['similars']:
-    #         self.movie_ids.add(similar['id'])
-
-    #     return self.movie_data, self.movie_ids
 

1e281ce32d0096be5a5c25ae9dcd2af483166dc3
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2022-08-10 23:47:34 -0400

    Fixed pronoun and verb replace functions
    The pronoun replace function was only replacing the last pronoun in the sentence, and it turns out the verb function was doing the same. I added the recursion back into the verb function and updated the pronoun function to have recursion as well. Also broke out the pronoun type counter into its own function.

--- fcm_dashApp/fcm_code/fortune_cookie.py
+++ fcm_dashApp/fcm_code/fortune_cookie.py
@@ -7,6 +7,7 @@ import textacy
 from textacy import extract
 import language_tool_python
 import pickle
+import re
 # from fcm_code import test_cases
 
 # Import the data
@@ -25,7 +26,31 @@ pronouns_dict = {"third_male":["he", "him", "his", "himself"]
                                 ,"themselves"]}
 
 
-def pronoun_replace(original_document):
+
+def count_pronoun_types(original_document):
+    nlp_doc = nlp(original_document)
+
+    ######################
+    # First chunk is seeing how many different types of pronouns are present
+    pronoun_types_present = []
+
+    for token in nlp(nlp_doc):
+        if token.pos_ == "PRON":
+            # print(token)
+            for key, val in pronouns_dict.items():
+                # print(key)
+                # print(val)
+                if str(token) in val:
+                    pronoun_types_present.append(key)
+
+    unique_pronoun_types_present = set(pronoun_types_present)
+
+    # if len(unique_pronoun_types_present)>1:
+    #     return original_document
+
+    return len(unique_pronoun_types_present)
+
+def pronoun_replace(original_document, i=0):
     '''Replace a single family of pronouns with second-person pronouns.
 
     Note: This function currently only replaces pronouns in sentences containing
@@ -42,38 +67,33 @@ def pronoun_replace(original_document):
 
     nlp_doc = nlp(original_document)
 
-    pronoun_types_present = []
+    pronoun_count = 0
+    for token in nlp_doc:
+        if spacy.explain(token.tag_) in ["pronoun, possessive", "pronoun, personal"]:
+            pronoun_count +=1
 
-    for token in nlp(nlp_doc):
-        if token.pos_ == "PRON":
-            # print(token)
-            for key, val in pronouns_dict.items():
-                # print(key)
-                # print(val)
-                if str(token) in val:
-                    pronoun_types_present.append(key)
-
-    if len(pronoun_types_present)>1:
+    if i == pronoun_count:
         return original_document
 
-    # use loop to replace diff types of pronouns, similar to verb_replace
-    working_doc = ""
+    # Loop replaces diff types of pronouns, similar to verb_replace
     for token in nlp_doc:
         token_tag = spacy.explain(token.tag_)
 
-        if token_tag == "pronoun, possessive":
-            working_doc = original_document.replace(str(token), "your")
+        if (token_tag == "pronoun, possessive"
+            and str(token) != "your"):
+            working_doc = re.sub(r'\b' + str(token) + r'\b', "your", original_document)
+            i+=1
+            return pronoun_replace(working_doc, i)
         
-        # this may overwrite "you", so I'd need to put it before the
-        # noun_replace function
-        if token_tag == "pronoun, personal":
-            working_doc = original_document.replace(str(token), "yourself")
-                
-    # Only return if replacements have been made
-    if len(working_doc) > 0:
-        return working_doc
+        if (token_tag == "pronoun, personal"
+            and str(token) != "yourself"):
+            # working_doc = original_document.replace(str(token), "yourself")
+            working_doc = re.sub(r'\b' + str(token) + r'\b', "you", original_document)
+            i+=1
+            return pronoun_replace(working_doc, i)
 
     return original_document
+
 
 def noun_replace(original_document):
     '''Returns a list of documents. Noun chunks that meet the specified
@@ -108,7 +128,7 @@ def noun_replace(original_document):
     
     return noun_mod_docs
 
-def verb_replace(original_document):
+def verb_replace(original_document, i=0):
     '''Returns a document replacing verbs meeting specified criteria with the
     structure "will {lemmatized verb}"
     
@@ -116,53 +136,39 @@ def verb_replace(original_document):
     ----------
     original_document : str
         Document in which you would like to replace verbs
+    i : int
+        Counter set to 0. I should put it in the function instead. I'll do that
+        later because right now it's working, and changing that will probably
+        magically break it... haha (sort of)
     '''
-
     nlp_doc = nlp(original_document)
 
     verb_count = 0
     for token in nlp_doc:
         if token.pos_ in ['VERB', 'AUX']:
-            verb_count += 1
+            verb_count +=1
+    # print("vc:",verb_count)
+    # print("i:",i)
 
-    if verb_count == 0:
+    if i == verb_count:
         return original_document
-    
+
     for token in nlp_doc:
+        # print(token)
+        # print(spacy.explain(token.tag_), '\n')
         token_tag = spacy.explain(token.tag_)
 
-        # Creating bail out statements (fail conditions) 
+        if (token.pos_ in ['VERB', 'AUX']
+            and token_tag in ["verb, 3rd person singular present"
+                             ,"verb, non-3rd person singular present"]
+            and token.dep_ != 'advcl'):
+            # This will not work for adverbial clauses, so add that logic in later
+            # working_doc = original_document.replace(str(token),f"will {token.lemma_}")
+            working_doc = re.sub(r'\b' + str(token) + r'\b', f"will {token.lemma_}", original_document)
+            # print("Working Doc:",working_doc)
+            i+=1
+            return verb_replace(working_doc, i)
 
-        # If the token in question isn't a verb or auxiliary verb
-        # continue on to the next token in the document
-        if token.pos_ not in ['VERB', 'AUX']:
-            continue
-        
-        # If the token tag isn't a 3rd person singular present tense verb
-        # or non-3rd person singular present tense verb, continue onward.
-        if token_tag not in ["verb, 3rd person singular present"
-                             , "verb, non-3rd person singular present"]:
-            # print(f"Not a verb in 3P: Token =  \'{token}\'")
-            continue
-
-        # If the token dependency is an adverbial clause, we want to bail 
-        # out early for now as this will not work for adverbial clauses; 
-        # logic will be added in later
-        if token.dep_ == 'advcl':
-            continue
-
-        # Now that the fail conditions are checked, 
-        # we can directly use the original logic.
-        # This logic will loop through the tokens in order
-        # meaning we no longer need to increment i. 
-        working_doc = original_document.replace(str(token),
-                                                f"will {token.lemma_}")
-                
-        # Only return if replacements have been made
-        if len(working_doc) > 0:
-            return working_doc
-
-    # Return the original if no replacements have been made.
     return original_document
 
 def verb_replace_advcl(original_document):
@@ -227,27 +233,39 @@ if __name__ == "__main__":
     for k in movie_data:
         plot = movie_data[k]['plot']
     # for plot in test_cases.EXAMPLE_SENTENCES:
-        pronouns_replaced = pronoun_replace(plot)
-        nouns_replaced = noun_replace(pronouns_replaced)
+        unique_pronoun_types = count_pronoun_types(plot)
+        if unique_pronoun_types <= 1:
+            pronouns_replaced = pronoun_replace(plot)
+            nouns_replaced = noun_replace(pronouns_replaced)
 
-        current_plot_outputs = []
-        for noun_plots in nouns_replaced:
-            verbs_replaced = verb_replace(noun_plots)
-            verbs_replaced = verb_replace_advcl(verbs_replaced)
-            current_plot_outputs.append(verbs_replaced)
+            current_plot_outputs = []
+            for noun_plots in nouns_replaced:
+                verbs_replaced = verb_replace(noun_plots)
+                verbs_replaced = verb_replace_advcl(verbs_replaced)
+                current_plot_outputs.append(verbs_replaced)
+                print(verbs_replaced,"\n----------")
 
-        all_outputs.append(current_plot_outputs)
+            all_outputs.append(current_plot_outputs)
 
-    # tool.close()
-
-
-    for i in all_outputs:
-        for j in i:
-            print(j,"\n----------")
-
-
+    # for i in all_outputs:
+    #     for j in i:
+    #         print(j,"\n----------")
 
 
+# plot = 'Gru meets his long-lost, charming, cheerful, and more successful twin brother Dru, who wants to team up with him for one last criminal heist.'
+
+# unique_pronoun_types = count_pronoun_types(plot)
+# if unique_pronoun_types <= 1:
+#     pronouns_replaced = pronoun_replace(plot)
+# nouns_replaced = noun_replace(pronouns_replaced)
+
+
+# # current_plot_outputs = []
+# for noun_plots in nouns_replaced:
+#     verbs_replaced = verb_replace(noun_plots)
+#     verbs_replaced = verb_replace_advcl(verbs_replaced)
+#     print(verbs_replaced)
+#     # current_plot_outputs.append(verbs_replaced)
 
         
 
@@ -255,7 +273,7 @@ if __name__ == "__main__":
 
 
 
-# for token in nlp(all_outputs[2][0]):
+# for token in nlp(plot):
 #      print (token, token.lemma_, token.dep_, token.tag_, token.pos_
 #             , spacy.explain(token.tag_), token.head)
 

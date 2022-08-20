dab0de792659b8bb8f1c37de823f0625d4424d30
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2022-08-20 00:34:54 -0400

    Copy updates, added links

--- fcm_dashApp/pages/about.py
+++ fcm_dashApp/pages/about.py
@@ -10,23 +10,25 @@ except ModuleNotFoundError:
 
 layout = html.Div([
             html.Div(id='center_content_container', children=[
-                html.P("...but we'll tell you a bit about it anyway."),
+                html.P("...but we'll tell you about it anyway.", style={'text-align':'center'}),
                 html.Br(),
+                dcc.Markdown("**What if fortune cookies were filled with movie plots?**"),
+                html.Br(),
+                # html.P("You've probably wondered that many times. Or maybe you haven't - that's fine too."),
+                # html.Br(),
                 dcc.Markdown("""
-
-                        **What if fortune cookies were filled with movie plots?**
-
-                        You've probably wondered that many times. Or maybe you haven't - that's fine too.
-
-                        This repo is a project currently in progress to create some sort of application using spaCy to transform brief movie synopses into fortune cookie statements and, hopefully, a web app that will be fun to use.
-
-                        Thanks for visiting!
+                
+                        Grab some extra buttery popcorn and test your movie knowledge with a [fortune cookie movie quiz](/quiz) or tempt fate with a [random movie fortune](/random).
                         
                         """),
                 html.Br(),
-                html.P("This app is a work in progress, so let us know if anything isn't working or if you "
-                       "have suggestions for additional features that could be added. "
-                       ),
+                html.P("Thanks for visiting!"),  # Adventure awaits! 
+                html.Br(),
+                dcc.Markdown("""Got feedback? Give us a shout!
+                            Create an issue in the [GitHub repo](https://github.com/EricPostMaster/fortune-cookie-movies), or even better - submit a Pull Request!
+                            """
+                            # This project uses spaCy to transform brief movie summaries into fortune cookie messages that will almost certainly come true...
+                            ),
                 html.Br(),
                 
                 # html.P("Idea and NLP code from")
